547014a61d4bce0f31b9c36fd9f6ed61c16c3ee6
Jieun <ji.eun.lee@kt.com>
2021-05-24 17:53:07 +0900

    add stack/queue 3 problem

--- truck.js
+++ truck.js
@@ -1,13 +1,36 @@
 
 /**
  * https://programmers.co.kr/learn/courses/30/lessons/42583
- * @param bridge_length 다리길이 (1~10000)
- * @param weight 다리가 버티는 무게 (1~10000)
- * @param truck_weights 트럭 무게 (1~weight, length:1~10000)
+ * @param {Number} bridge_length 다리길이 (1~10000)
+ * @param {Number} weight 다리가 버티는 무게 (1~10000)
+ * @param {Array} truck_weights 트럭 무게 (1~weight, length:1~10000)
  * @returns 모든 트럭이 다리를 건너는 시간
  * 트럭은 1초에 1만큼 움직인다.
  */
 function solution(bridge_length, weight, truck_weights) {
-    var answer = 0;
-    return answer;
+    let truck_on_bridge = [], curr_weight = 0;
+    let curr_time = 0;
+    while(truck_weights.length > 0) {
+        if (truck_on_bridge.length >= bridge_length) {
+            const crossed = truck_on_bridge.shift();
+            curr_weight -= crossed;
+        }        
+        if (curr_weight + truck_weights[0] <= weight && truck_on_bridge.length < bridge_length) {
+            const crossing = truck_weights.shift();
+            truck_on_bridge.push(crossing);
+            curr_weight += crossing;
+        }
+        else {
+            truck_on_bridge.push(0);
+        }        
+        if (truck_weights.length == 0) {
+            curr_time += bridge_length;
+        }        
+        curr_time++;        
+    }
+    return curr_time;
 }
+
+console.log(solution(2, 10, [7,4,5,6]));
+console.log(solution(100, 100, [10]));
+console.log(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]));
